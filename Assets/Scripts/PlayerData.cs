1e0186adfee0770cb42293031e7d38e95dd38b0e
Erdem_Arslann <erdemarslan7859@gmail.com>
2020-09-10 19:38:29 +0300

    player data added

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlayerData 
+{
+    public int level;
+    public int highScore; // the score depends on time    
+}
